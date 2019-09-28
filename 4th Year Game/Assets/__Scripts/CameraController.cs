90090c461a6ffcfc98515da900dd22833b7e3c33
NeilK-94 <neil.kyne@gmail.com>
2019-09-28 15:28:50 +0100

    Added a simple camera controller cript to follow the player

---
+++ 4th Year Game/Assets/__Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //add player to this tranform layer
+    public Transform player;
+
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        //  Get the x and y positions of the players transform
+        var getPlayerX = player.transform.position.x;
+        var getPlayerY = player.transform.position.y;
+        transform.position = new Vector3(getPlayerX, getPlayerY,-1);    //  Set the position of the transform(camera)
+        //Debug.Log(getPlayerX);     
+    }
+}
