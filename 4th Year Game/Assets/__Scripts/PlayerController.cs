11ad5fb483f62984be0f81fca7efc83cad874bc5
NeilK-94 <neil.kyne@gmail.com>
2019-09-28 14:13:48 +0100

    Added a temporary player while waiting on assets. Have a player controller script with basic movement working

---
+++ 4th Year Game/Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float moveSpeed;
+    public float jumpHeight;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   //  Reference to the rigidbody2d component
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) //  Check if space bar is pressed
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+        if (Input.GetKey(KeyCode.D)) //  Check if space bar is pressed
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.A)) //  Check if space bar is pressed
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+    }
+}
