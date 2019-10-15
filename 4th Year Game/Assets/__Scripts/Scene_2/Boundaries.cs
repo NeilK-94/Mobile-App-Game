8f697b44379249f63d520ca7578d8e94814b3090
NeilK-94 <neil.kyne@gmail.com>
2019-10-15 20:53:59 +0100

    Added level 2.
    Started work on scene 2 which is a top down level. Have added a new player movement script as he will need to move vertically. Also added a boundaries script to stop the player leaving the camera.

---
+++ 4th Year Game/Assets/__Scripts/Scene_2/Boundaries.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    // get the main camera
+    // get the bounds of it (X,Y) - half the size
+    // use Mathf.Clamp to compare the min, max to the current x
+
+    // == public properties ==
+    public Camera mainCamera;
+
+    // == private fields ==
+    private Vector2 screenBound;
+    private float width;
+    private float height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // find the bounds from the camera
+        screenBound = mainCamera.ScreenToWorldPoint(
+                                new Vector3(Screen.width,
+                                            Screen.height,
+                                            mainCamera.transform.position.z));
+        Debug.Log("Screen width = " + screenBound.x);
+        width = gameObject.GetComponent<SpriteRenderer>().bounds.extents.x;
+        height = gameObject.GetComponent<SpriteRenderer>().bounds.extents.y;
+    }
+
+    // after the player moves, need to check that they are in bounds
+    // player moves on the update method.  Can't use the update - need to 
+    // ensure this is done after Update -> LateUpdate
+    private void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        // find the position of X and clamp
+        // find the position of Y and clamp
+        viewPos.x = Mathf.Clamp(viewPos.x,
+                                screenBound.x * -1 + width,
+                                screenBound.x - width);
+        viewPos.y = Mathf.Clamp(viewPos.y,
+                                screenBound.y * -1 + (width * 0.8f),
+                                screenBound.y - height);
+
+        transform.position = viewPos;
+    }
+}
---
+++ 4th Year Game/Assets/__Scripts/Scene_2/PlayerController_2.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController_2 : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    private float movementHorizontal = 0.0f;
+    private float movementVertical= 0.0f;
+
+    Vector2 movement;
+
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   //  Reference to the rigidbody2d component
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+
+        if(movement.x < 0.0f)
+        {
+            transform.rotation = Quaternion.Euler(Vector3.forward * -90);
+        }
+        else if(movement.x > 0.0f)
+        {
+            transform.eulerAngles = Vector3.forward * 90;
+        }
+
+        if (movement.y < 0.0f)
+        {
+            transform.eulerAngles = Vector3.forward * 0;
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        else if (movement.y > 0.0f)
+        {
+            transform.eulerAngles = Vector3.forward * 0;
+            transform.localScale = new Vector2(1f, -1f);
+        }
+
+        //movementHorizontal = Input.GetAxis("Horizontal");
+        //movementVertical = Input.GetAxis("Vertical");
+
+        //if (movementHorizontal > 0.0f)
+        //{
+        //    rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        //    transform.eulerAngles = Vector3.forward * 90;  //  If moving right set local rotation to 90
+        //}
+
+        //else if (movementHorizontal < 0.0f)
+        //{
+        //    rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        //    transform.rotation = Quaternion.Euler(Vector3.forward * -90);    //  If moving left change the rotation to -90
+        //}
+        //else
+        //{
+        //    rb.velocity = new Vector2(0f, rb.velocity.y);   //  Sets player horizontal movement to 0, this prevents sliding
+
+        //}
+
+        //if (movementVertical > 0.0f)
+        //{
+        //    transform.eulerAngles = Vector3.forward * 0;
+        //    rb.velocity = new Vector2(rb.velocity.x, moveSpeed);
+        //    transform.localScale = new Vector2(1f, -1f); //  If moving up set local scale to -1 on the Y thus flipping
+        //}
+
+        //else if (movementVertical < 0.0f)
+        //{
+        //    transform.eulerAngles = Vector3.forward * 0;
+        //    rb.velocity = new Vector2(rb.velocity.x, -moveSpeed);
+        //    transform.localScale = new Vector2(1f, 1f);    //  If moving down change the scale on the y axis to 1
+        //}
+        //else
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, 0f);   //  Sets player vertical movement to 0, this prevents sliding
+
+        //}
+
+
+    }
+}
