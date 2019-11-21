d14d0b593ad523737a4382f2f3659f7044ab3ad5
NeilK-94 <neil.kyne@gmail.com>
2019-11-21 19:29:24 +0000

    Added random colors to boxes as well as tiled background

--- 4th Year Game/Assets/__Scripts/ButtonScript.cs
+++ 4th Year Game/Assets/__Scripts/ButtonScript.cs
@@ -1,27 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
     public GameObject Box;
     public Transform spawnpoint1;
-    public Transform spawnpoint2;
     public Rigidbody2D player;
     public Renderer boxRenderer;
 
     [SerializeField]
-    private Color boxColor;
-    [SerializeField]
     private AudioSource actionSound;
-
-    // Update is called once per frame
-    private void Start()
-    {
-        //boxRenderer = Box.gameObject.GetComponent<Renderer>();
-        boxRenderer = GameObject.Find("Box").GetComponent<Renderer>();  //  Find the renderer of the box prefab
-    }
-
 
     public void OnTriggerStay2D(Collider2D col)
     {
@@ -29,12 +16,30 @@ public class ButtonScript : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                //  Instantiate a box at the spawpoint
                 Box = Instantiate(Box, spawnpoint1.position, Quaternion.identity);
-                boxRenderer = Box.transform.GetComponent<Renderer>();
-                boxRenderer.material.color = boxColor;  //  change the color after it is instantiated
-				//	box.tag = "redBox";
-				//	or
-				//	if(boxcolor == red){	box.tag = "redBox"}
+                //  Set boxRenderer to be the Renderer component of the instantiated box
+                boxRenderer = Box.GetComponent<Renderer>(); //  box.tranform
+                
+                //  Get a random number between 1 and 3, this will be used to assign the boxes thier color and tag
+                int rndColor = Random.Range(1, 4);
+                //Debug.Log(rndColor);
+
+                if (rndColor == 1)
+                {
+                    boxRenderer.material.color = Color.red;  //  change the color after it is instantiated
+                    Box.tag = "redBlock";   //  Set it's tag
+                }
+                else if (rndColor == 2)
+                {
+                    boxRenderer.material.color = Color.blue;
+                    Box.tag = "blueBlock";
+                }
+                else
+                {
+                    boxRenderer.material.color = Color.green;
+                    Box.tag = "greenBlock";
+                }
                 actionSound.Play();
             }
         }
